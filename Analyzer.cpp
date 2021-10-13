f807e34fbb4aeb4b13ff0fcd00ba252e5d9e47f0
Matthew Huynh <83797785+mattchooo@users.noreply.github.com>
2021-10-12 23:14:05 -0400

    Add files via upload

--- Analyzer.cpp
+++ Analyzer.cpp
@@ -1,86 +1,52 @@
-#include <iostream>
-
+// CPP program to implement Binary Search for strings
+#include<bits/stdc++.h>
 using namespace std;
-#include "StringData.h"
 
-#include <cstdlib>
-
-#include <string>
-#include <vector>
-#include <chrono>
-
-
-int linearSearch(vector<string> dataSet, string element)
+// Returns index of x if it is present in arr[],
+// else return -1
+int binarySearch(string arr[], string x,int n)
+{
+    int l = 0 ;
+    int r = n - 1;
+    while (l <= r)
     {
+        int m = l + (r - l) / 2;
 
-        int lenData = dataSet.size();
-        //Iterate through every index and see if the element is equal to the element at some index in the dataset.
-        for(int i=0;i<lenData;i++)
-        {
-            if(element==(dataSet[i]))
-            {
-                cout << "The Linear Search found the element at index " << i << endl;
-                return i;
+        int res = -1000; //some random value assigned because if res is already 0 then
+        //it will always return 0
+        if (x == (arr[m]))
+            res = 0;
 
-            }
-        }
-        printf("The Linear Search did not find the element");
-        return -1;
+
+        // Check if x is present at mid
+        if (res == 0)
+            return m;
+
+        // If x greater, ignore left half
+        if (x > (arr[m]))
+            l = m + 1;
+
+            // If x is smaller, ignore right half
+        else
+            r = m - 1;
     }
-int binarySearch(vector <string> dataSet, string element)
-    {
-        int lenData = dataSet.size();
-        int minIndex=0;
-        int highIndex=lenData;
-        int mid;
-        //Uses the formulas given in lecture to perform binary search. Return the mid value if it is the element.
-        //If the element is greater than the mid of the dataset, make the min the mid+1
-        //If the element is less than the mid of the dataset, make the max, the mid-1.
-        while(minIndex <= highIndex)
-        {
-            mid=(minIndex+(highIndex-minIndex)/2);
-            if (element==dataSet[mid])
-            {
-                cout << "The Binary Search found the element at index " << mid << endl;
-                return mid;
 
+    return -1;
+}
 
-            }
-            if (element.compare(dataSet[mid]) > 0)
-            {
-                minIndex=mid+1;
-            }
-            if (element.compare(dataSet[mid]) < 0)
-            {
-                highIndex=mid-1;
-            }
-        }
-        printf("The Binary Search did not find the element");
-        return -1;
-
-
-    }
+// Driver code
 int main()
-    {   //Scan in the element we want
-        string element;
-        cout << "Enter the element you want to search for: ";
-        cin >> element;
-        //Get the set of datsets
-        vector<string> data=  getStringData();
-        //Check the time the binary search takes
-        //Use the given function for time
-        long firstTime1 = systemTimeNanoseconds();
-        int result1;
-        result1 = binarySearch(data, element);
-        long currentTime1 = systemTimeNanoseconds();
-        long lastTime1 = currentTime1 - firstTime1;
+{
+    string arr[] = { "contribute", "geeks", "ide", "practice"};
+    string x = "ide";
+    int n = 4;
+    int result = binarySearch(arr, x,n);
 
-        cout << "The Binary Search took " << lastTime1 << endl;
-        //Check the time the linear search takes
-        long firstTime2 = systemTimeNanoseconds();
-        int result2;
-        result2 = linearSearch(data, element);
-        long currentTime2 = systemTimeNanoseconds();
-        long lastTime2 = currentTime2 - firstTime2;
-        cout << "The Linear Search took " << lastTime2 << endl;
-    }
+    if (result == -1)
+        cout << ("Element not present");
+    else
+        cout << ("Element found at index ") << result;
+}
+
+// This code is contributed by
+// Shashank_Sharma
